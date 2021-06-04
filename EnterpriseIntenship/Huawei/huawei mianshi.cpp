24b5e3f752b78ef5809c26106a9dbcf0eb437a42
LukeLIN-web <1263810658@qq.com>
2021-06-04 16:03:31 +0800

    huawei interview is leetcode13

--- EnterpriseIntenship/Huawei/huawei mianshi.cpp
+++ EnterpriseIntenship/Huawei/huawei mianshi.cpp
@@ -1,3 +1,23 @@
+// class Solution:
+//     def romanToInt(self, s: str) -> int:
+//         dict = { "I":1, "V":5,"X":10,"L":50,"C":100,"D":500,"M":1000,"IV":4,"IX":9,"XC":90,"XL":40, "CM":900, "CD":400}
+//         res = 0
+//         n = len(s)
+//         i = 0
+//         while i < n:
+//             if i +1 < n:
+//                 tmp = s[i] + s[i+1]
+//                 i +=1 
+//                 print(tmp)
+//                 if tmp not in dict:
+//                     print(tmp)
+//                     i -=1
+//                     tmp = s[i]
+//             else:
+//                 tmp =  s[i]
+//             res += dict[tmp]
+//             i +=1
+//         return res
 #include<iostream>
 using namespace std;
 int main(){
